e9bfbee682f855223148a93a39788b2fd1363f55
s1Sharp <maksim.carkov.201300@gmail.com>
2022-05-06 00:48:00 +0300

    sorting: random vector generator

---
+++ src/sorting/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include "sorting.h"
+
+#define TYPE float
+
+int main()
+{
+    std::vector<float> v(10);
+    rangom_vector(v);
+    
+    return 0;
+}
---
+++ src/sorting/sorting.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include "../timer_util.h"
+
+
+template <typename T>
+void rangom_vector(std::vector<T>& v)
+{
+    using std::setprecision;
+
+
+    const T FLOAT_MAX = static_cast<T>(v.capacity() / 2);
+    const T FLOAT_MIN = -static_cast<T>(v.capacity() / 2);
+
+    std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_real_distribution<> distr(FLOAT_MIN, FLOAT_MAX);
+    //distr(eng)
+
+    generate(v.begin(), v.end(), 
+            [&eng, &distr] () mutable  {
+                auto dist = distr(eng);
+                //std::cout << dist << std::endl;
+                return dist;
+            });
+
+    return;
+}
