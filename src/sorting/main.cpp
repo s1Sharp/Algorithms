f7843336083a255440c94f1f41eca27f2ebaf222
s1Sharp <maksim.carkov.201300@gmail.com>
2022-05-07 18:25:40 +0300

    selectionSort implementation

--- src/sorting/main.cpp
+++ src/sorting/main.cpp
@@ -3,12 +3,61 @@
 #include <ctime>
 #include "sorting.h"
 
+#define STRINGIZE(x) #x
+
 #define TYPE float
+#define SIZE 10000
+
+template <typename _T>
+void selectionSort(std::vector<_T>& v, size_t n = 0)
+{   
+    assert(n >= 0);
+    if (n == 0 && n == (v.end() - v.begin()))
+    {
+        return;
+    }
+
+    n = static_cast<size_t>(v.end() - v.begin());
+
+    size_t i, j, min_idx;
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n - 1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i + 1; j < n; j++) 
+        if (v[j] < v[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+        std::swap(v[min_idx], v[i]);
+    } 
+};
+
+auto benchmark = [](auto v,  auto f, auto sort_type) -> double
+{
+    static Timer timer;
+    timer.start();
+    f(v, 0);
+    timer.stop();
+
+    // asserting vector sorting
+    bool is_sorted = std::is_sorted(std::begin(v), std::end(v));
+    assert(is_sorted);
+    is_sorted ? std::cout << "sorted by " << sort_type << std::endl : std::cout << "not sorted by " << sort_type << std::endl;
+    double time =  timer.elapsedMilliseconds();
+    return time;
+};  
+
 
 int main()
 {
-    std::vector<float> v(10);
+
+    std::vector<TYPE> v(SIZE);
     rangom_vector(v);
+
+    std::cout<< "elapsed time " << benchmark(v, selectionSort<TYPE>, STRINGIZE(selectionSort)) << " ms" << std::endl;
     
     return 0;
 }
--- src/sorting/sorting.h
+++ src/sorting/sorting.h
@@ -3,19 +3,41 @@
 #include <random>
 #include <iomanip>
 #include <vector>
+#include <iterator>
 #include <algorithm>
 #include <ctime>
+#include <functional>
+#include <utility>
+#include <assert.h>
 #include "timer_util.h"
 
 
-template <typename T>
-void rangom_vector(std::vector<T>& v)
+
+
+
+template <typename _T, typename SortCallFunc> // typename _Comparator>
+double sort_vector(std::vector<_T> v, SortCallFunc sotring)
 {
+    t.start();
+    // sortings call
+    sotring(std::begin(v), std::end(v));
+    t.end();
+
+    // asserting vector sorting
+    assert(std::is_sorted(std::begin(v), std::end(v)))
+
+    return t.elapsedMilliseconds();
+}
+
+template <typename _T>
+void rangom_vector(std::vector<_T>& v)
+{
+
     using std::setprecision;
 
 
-    const T FLOAT_MAX = static_cast<T>(v.capacity() / 2);
-    const T FLOAT_MIN = -static_cast<T>(v.capacity() / 2);
+    const _T FLOAT_MAX = static_cast<_T>(v.capacity() / 2);
+    const _T FLOAT_MIN = -static_cast<_T>(v.capacity() / 2);
 
     std::random_device rd;
     std::default_random_engine eng(rd());
