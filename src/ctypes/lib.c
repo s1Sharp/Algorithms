4675093a57b404df185aa7ab519d8dce683221d3
Maksim 's1Sharp' Tsarkov <maksim.carkov.201300@gmail.com>
2022-08-21 00:01:27 +0300

    ctypes usage variable by reference

--- src/ctypes/lib.c
+++ src/ctypes/lib.c
@@ -7,23 +7,26 @@ int non_atomic_counter;
 
 int mythreads(void *thr_data)
 {
-    (void)thr_data;
-    for(int n = 0; n<20000000; ++n) {
+    int c = *(int *)thr_data;
+    for(int n = 0; n<c; ++n) {
         ++non_atomic_counter;
         ++atomic_counter;
     }
     return 0;
 }
 
-int start_thread() {
+int start_thread(int* counter) {
     thrd_t thr[10];
     for(int n=0; n<10; ++n) {
-        thrd_create(&thr[n], mythreads, NULL);
+        thrd_create(&thr[n], mythreads, (void *)counter);
     }
     for(int n=0; n<10; ++n) {
         thrd_join(thr[n], NULL);
     }
     printf("atomic_counter %d\n", atomic_counter);
     printf("counter %d\n", non_atomic_counter);
+
+    *counter = 5;
+
     return 0;
 }
--- src/ctypes/main.py
+++ src/ctypes/main.py
@@ -2,4 +2,10 @@ import ctypes
 
 lib1 = ctypes.CDLL("./lib.so")
 
-lib1.start_thread()
+counter = 10
+c_counter = ctypes.c_int(counter)
+
+lib1.start_thread(ctypes.byref(c_counter))
+
+counter = c_counter.value
+print(counter)
